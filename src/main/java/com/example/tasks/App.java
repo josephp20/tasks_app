156f0686d54db48ae96e08fac5cbb5889a6df2a8
Jose Sebastian Manzano <jose2002sebas@gmail.com>
2026-07-01 15:53:33 -0400

    Initial commit

---
+++ src/main/java/com/example/tasks/App.java
@@ -0,0 +1,13 @@
+package com.example.tasks;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Tasks Application");
+        System.out.println("Build version 2.0");
+        System.out.println("Features:");
+        System.out.println("- Add a task");
+        System.out.println("- Delete a task");
+        System.out.println("- View tasks");
+        System.out.println("- Mark a task as EDITED");
+    }
+}
